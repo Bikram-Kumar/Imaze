0c8541a8706a7bce319abb1de0a4bb5d092f104a
Bikram Kumar <coolbikram0@gmail.com>
2024-09-25 13:16:17 +0530

    added color to grayscale

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import {Vector2} from "./modules/Vector2.js"
 import {MazeManager} from "./modules/MazeManager.js"
+import { ImageHandler } from "./modules/ImageHandler.js";
 
 window.onload = main;
 
@@ -10,7 +11,7 @@ function main() {
 
 
     var canvasDim = new Vector2(256, 256);
-    var mazeDim = new Vector2(128, 128);
+    var mazeDim = new Vector2(16, 16);
     
     var canvas = document.getElementById("cnvs");
     var ctx = canvas.getContext("2d");
@@ -21,34 +22,25 @@ function main() {
     var imgData = ctx.getImageData(0, 0, canvasDim.x, canvasDim.y);
     
 
-    var mask = new Array(mazeDim.x*mazeDim.y);
 
     var factor = canvasDim.x / mazeDim.x;
 
-    for (let i = 0; i < mask.length; i++) {
-        if (imgData.data[4*i*factor] > 127) {
-            mask[i] =  true;
-        } else {
-            mask[i] =  false;
 
-        }
-    }
+    var mask = ImageHandler.getImageMask(ImageHandler.getImageData(image, mazeDim));
 
-
+    imgData = ImageHandler.turnGrayscale(imgData);
     ctx.putImageData(imgData, 0, 0);
     
     var mazeManager = new MazeManager(mazeDim, mask);
-    console.log(imgData);
-    console.log(mazeManager.mask);
 
-    ctx.fillStyle = "#00ff00";
-    for (let i = 0; i < mask.length; i++) {
-        if (mask[i]) {
-            let x = (i % mazeDim.x) * factor;
-            let y = Math.floor(i / mazeDim.x) * factor;
-            ctx.fillRect(x, y, factor, factor);
-        }
-    }
+    // ctx.fillStyle = "#ffffff";
+    // for (let i = 0; i < mask.length; i++) {
+    //     if (mask[i]) {
+    //         let x = (i % mazeDim.x) * factor;
+    //         let y = Math.floor(i / mazeDim.x) * factor;
+    //         ctx.fillRect(x, y, factor, factor);
+    //     }
+    // }
 
 }
 
--- modules/ImageHandler.js
+++ modules/ImageHandler.js
@@ -1,7 +1,62 @@
 export class ImageHandler {
 
-    constructor () {
+    static ctx = undefined;
 
+
+    static getImageData(image, size) {
+        if (this.ctx == undefined) {
+            this.ctx = document.createElement("canvas").getContext("2d");
+        }
+
+        this.ctx.canvas.width = size.x;
+        this.ctx.canvas.height = size.y;
+
+        this.ctx.drawImage(image, 0, 0, size.x, size.y);
+        return (this.ctx.getImageData(0, 0, size.x, size.y));
+        
+    }
+
+
+    static turnGrayscale(imageData) {
+        var gdata = new ImageData(imageData.width, imageData.height);
+
+        for (let i = 0; i < imageData.data.length; i+=4) {
+            gdata.data[i] = (0.2126 * this.srgbToLinear(imageData.data[i])) +
+                (0.7152 * this.srgbToLinear(imageData.data[i+1])) +
+                (0.0722 * this.srgbToLinear(imageData.data[i+2]));
+
+            gdata.data[i+1] = gdata.data[i];
+            gdata.data[i+2] = gdata.data[i];
+            gdata.data[i+3] = 255;
+        }
+
+        return gdata;
+
+    }
+
+
+    static getImageMask(imageData) {
+       
+        var mask = new Array(imageData.data.length / 4);
+    
+    
+        for (let i = 0; i < mask.length; i++) {
+            if (imageData.data[4*i] > 127) {
+                mask[i] =  true;
+            } else {
+                mask[i] =  false;
+
+            }
+        }
+
+        return mask;
+    
+    }
+
+    static srgbToLinear(channel) {
+        channel /= 255;
+        if (channel <= 0.04045) return (255 * channel / 12.92);
+        return (255 * Math.pow((channel + 0.055) / 1.055, 2.4));
     }
 
 
