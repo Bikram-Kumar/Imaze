2e8d9efb690dce1b185f371078a8fe72c32d8e29
Bikram Kumar <coolbikram0@gmail.com>
2024-09-26 19:21:18 +0530

    added convolution

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function main() {
 
 
     var canvasDim = new Vector2(256, 256);
-    var mazeDim = new Vector2(16, 16);
+    var mazeDim = new Vector2(256, 256);
     
     var canvas = document.getElementById("cnvs");
     var ctx = canvas.getContext("2d");
@@ -28,19 +28,47 @@ function main() {
 
     var mask = ImageHandler.getImageMask(ImageHandler.getImageData(image, mazeDim));
 
-    imgData = ImageHandler.turnGrayscale(imgData);
+    // imgData = ImageHandler.turnGrayscale(imgData);
+
+    var kernelX = [
+        -1,0,1,
+        -2,0,2,
+        -1,0,1
+    ];
+    var kernelY = [
+        -1,-2,-1,
+        0,0,0,
+        1,2,1
+    ];
+    var kernelG = [
+        1,2,1,
+        2,4,2,
+        1,2,1
+    ];
+    var kernelR = [
+        0,-1,0,
+        -1,4,-1,
+        0,-1,0
+    ];
+    // imgData = ImageHandler.convolve(imgData, kernelX, 1);
+    // imgData = ImageHandler.convolve(imgData, kernelY, 1);
+    
+    imgData = ImageHandler.convolve(imgData, kernelR, 1);
+    mask = ImageHandler.getImageMask(imgData);
+    // imgData = ImageHandler.convolve(imgData, kernelG, 16);
+    console.log(imgData);
     ctx.putImageData(imgData, 0, 0);
     
     var mazeManager = new MazeManager(mazeDim, mask);
 
-    // ctx.fillStyle = "#ffffff";
-    // for (let i = 0; i < mask.length; i++) {
-    //     if (mask[i]) {
-    //         let x = (i % mazeDim.x) * factor;
-    //         let y = Math.floor(i / mazeDim.x) * factor;
-    //         ctx.fillRect(x, y, factor, factor);
-    //     }
-    // }
+    ctx.fillStyle = "#00ffff";
+    for (let i = 0; i < mask.length; i++) {
+        if (mask[i]) {
+            let x = (i % mazeDim.x) * factor;
+            let y = Math.floor(i / mazeDim.x) * factor;
+            ctx.fillRect(x, y, factor, factor);
+        }
+    }
 
 }
 
--- modules/ImageHandler.js
+++ modules/ImageHandler.js
@@ -17,13 +17,66 @@ export class ImageHandler {
     }
 
 
+    static convolveMask (mask, width, height, kernel, sumKernel) {
+        var cmask = new Array(mask.length);
+
+        for (let x = 1; x < width-1; x++) {
+            for (let y = 1; y < height-1; y++) {
+  
+                for (let kx = 0; kx < 3; kx++) {
+                    for (let ky = 0; ky < 3; ky++) {
+                        cmask[y*width + x] += kernel[(3 * ky) + kx] * mask[(width * (y - ky + 1)) + (x - kx + 1)];
+                    }
+                }
+                cmask[y*width + x] /= sumKernel;
+            }
+        }
+
+        return cmask;
+
+    }
+
+
+    
+    static convolve (imageData, kernel, sumKernel) {
+        var cdata = new ImageData(imageData.width, imageData.height);
+        let sumR, sumG, sumB;
+
+        for (let x = 1; x < imageData.width-1; x++) {
+            for (let y = 1; y < imageData.height-1; y++) {
+                sumR = 0;
+                sumG = 0;
+                sumB = 0;
+
+                for (let kx = 0; kx < 3; kx++) {
+                    for (let ky = 0; ky < 3; ky++) {
+                        sumR += kernel[(3 * ky) + kx] * imageData.data[4*((imageData.width * (y - ky + 1)) + (x - kx + 1))];
+                        sumG += kernel[(3 * ky) + kx] * imageData.data[4*((imageData.width * (y - ky + 1)) + (x - kx + 1)) + 1];
+                        sumB += kernel[(3 * ky) + kx] * imageData.data[4*((imageData.width * (y - ky + 1)) + (x - kx + 1)) + 2];
+                    }
+                }
+                cdata.data[4*((imageData.width * (y)) + (x))] = sumR / sumKernel;
+                cdata.data[4*((imageData.width * (y)) + (x)) + 1] = sumG / sumKernel;
+                cdata.data[4*((imageData.width * (y)) + (x)) + 2] = sumB / sumKernel;
+                cdata.data[4*((imageData.width * (y)) + (x)) + 3] = 255;
+
+            }
+        }
+
+        return cdata;
+
+    }
+
+
     static turnGrayscale(imageData) {
         var gdata = new ImageData(imageData.width, imageData.height);
 
         for (let i = 0; i < imageData.data.length; i+=4) {
-            gdata.data[i] = (0.2126 * this.srgbToLinear(imageData.data[i])) +
-                (0.7152 * this.srgbToLinear(imageData.data[i+1])) +
-                (0.0722 * this.srgbToLinear(imageData.data[i+2]));
+            gdata.data[i] = 255 * this.linearToSrgb(
+                (0.2126 * this.srgbToLinear(imageData.data[i] / 255)) +
+                (0.7152 * this.srgbToLinear(imageData.data[i+1] / 255)) +
+                (0.0722 * this.srgbToLinear(imageData.data[i+2] / 255))
+            );
 
             gdata.data[i+1] = gdata.data[i];
             gdata.data[i+2] = gdata.data[i];
@@ -53,10 +106,18 @@ export class ImageHandler {
     
     }
 
+    // channel in range 0-1
     static srgbToLinear(channel) {
-        channel /= 255;
-        if (channel <= 0.04045) return (255 * channel / 12.92);
-        return (255 * Math.pow((channel + 0.055) / 1.055, 2.4));
+        if (channel <= 0.04045) return (channel / 12.92);
+        return Math.pow((channel + 0.055) / 1.055, 2.4);
+    }
+    
+    
+    
+    // channel in range 0-1
+    static linearToSrgb(channel) {
+        if (channel <= 0.0031308) return (channel * 12.92);
+        return ((1.055 * Math.pow(channel, 1/2.4)) - 0.055);
     }
 
 
